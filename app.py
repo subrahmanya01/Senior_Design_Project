fea443b442e1aef261490f1aa5118ba0d5d0165f
unknown <subgb98@gmail.com>
2022-09-30 13:14:37 +0530

    added new

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
-from flask import Flask,render_template
+from flask import Flask, render_template,request
+import base64
+#from modelrun import run
+
+
 app = Flask(__name__)
 
-@app.route("/")
+# In this case, the URL route is 'displaylocations'.
+@app.route('/')
 def main():
-     return render_template("index.html")
+    return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/contribute")
+def contribute():
+    return render_template("location.html")
+
+
+@app.route("/api", methods=['GET','POST'])
+def api():
+    if request.method  =='GET':
+        return "NO PICTURE"
+    if request.method =='POST':
+        image_data = request.form.get("content").split(",")[1]
+        with open("static/Pothole/client_image.png","wb") as f:
+            f.write(base64.b64decode(image_data))
+        res  = True
+        if(res):
+            return "Potholes Detected"
+        else:
+            return "Good road"
+
+    
+    
+if __name__ == '__main__':
+    app.run(debug=True)
